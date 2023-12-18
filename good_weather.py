08ad2fb5c9c7df75e1ee66c8b9b15ab4cfb5e585
Kevin Balke <fughilli@gmail.com>
2023-12-17 23:19:10 -0800

    Add logging, format

--- good_weather.py
+++ good_weather.py
@@ -9,6 +9,7 @@ import requests
 import tqdm
 from scipy.interpolate import griddata
 
+
 def get_bounding_box(latitude, longitude, radius):
     # Earth's radius in miles
     R = 3958.8
@@ -34,6 +35,7 @@ def get_bounding_box(latitude, longitude, radius):
 
     return (min_lat, max_lat, min_lon, max_lon)
 
+
 # Example usage
 latitude = 37.7749  # Example latitude (New York City)
 longitude = -122.4194  # Example longitude (New York City)
@@ -41,7 +43,9 @@ radius = 500  # Radius in miles
 
 # Constants
 OPENWEATHERMAP_API_KEY = open("API_KEY").read().strip()
-LAT_MIN, LAT_MAX, LON_MIN, LON_MAX = get_bounding_box(latitude, longitude, radius)
+LAT_MIN, LAT_MAX, LON_MIN, LON_MAX = get_bounding_box(latitude, longitude,
+                                                      radius)
+
 
 # Step 1: Fetch Weather Data
 def fetch_weather_data(lat, lon):
@@ -49,6 +53,7 @@ def fetch_weather_data(lat, lon):
     response = requests.get(url)
     data = response.json()
     return data
+
 
 # Step 2: Compute Scalar Values
 def compute_scalar_values(weather_data):
@@ -61,6 +66,8 @@ def compute_scalar_values(weather_data):
         clouds = item['clouds']['all']
         sun_exposure = 1 - clouds / 100
 
+        temp = temp * 9 / 5 + 32
+
         weighted_temp = (temp - 40) / 30  # Scaled between 0 and 1
         weighted_sun_exposure = sun_exposure
 
@@ -72,15 +79,20 @@ def compute_scalar_values(weather_data):
     avg_sun_exposure = total_sun_exposure / count
 
     scalar_value = (avg_temp + avg_sun_exposure) / 2
-    return scalar_value
+    return scalar_value, avg_temp * 30 + 40, avg_sun_exposure  # returning actual average temp and sun exposure
+
 
 # Step 3: Bilinear Interpolation
 def interpolate_values(coordinates, values, num_points=100):
     latitudes = [coord[0] for coord in coordinates]
     longitudes = [coord[1] for coord in coordinates]
-    grid_x, grid_y = np.mgrid[LAT_MIN:LAT_MAX:num_points*1j, LON_MIN:LON_MAX:num_points*1j]
-    grid_z = griddata((latitudes, longitudes), values, (grid_x, grid_y), method='linear')
+    grid_x, grid_y = np.mgrid[LAT_MIN:LAT_MAX:num_points * 1j,
+                              LON_MIN:LON_MAX:num_points * 1j]
+    grid_z = griddata((latitudes, longitudes),
+                      values, (grid_x, grid_y),
+                      method='linear')
     return grid_x, grid_y, grid_z
+
 
 # Step 4 & 5: Fetch Map Data and Overlay Heatmap
 def create_heatmap(lat, lon, interpolated_values):
@@ -91,40 +103,51 @@ def create_heatmap(lat, lon, interpolated_values):
     grid_x, grid_y, grid_z = interpolated_values
 
     # Convert the grid data into (lat, lon, value) triplets for plotting
-    data = [
-        [grid_x[i, j], grid_y[i, j], grid_z[i, j]]
-        for i in range(grid_x.shape[0])
-        for j in range(grid_x.shape[1])
-        if not np.isnan(grid_z[i, j])
-    ]
+    data = [[grid_x[i, j], grid_y[i, j], grid_z[i, j]]
+            for i in range(grid_x.shape[0]) for j in range(grid_x.shape[1])
+            if not np.isnan(grid_z[i, j])]
 
     # Add heatmap
     folium.plugins.HeatMap(data).add_to(map_folium)
 
     return map_folium
 
+
 # Main Function
 def main():
     # Setup basic logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
     # Command line argument parsing
     parser = argparse.ArgumentParser(description='Generate a Weather Heatmap.')
-    parser.add_argument('--num-points', type=int, default=10, help='Number of points to sample in each dimension (default: 10)')
+    parser.add_argument(
+        '--num-points',
+        type=int,
+        default=10,
+        help='Number of points to sample in each dimension (default: 10)')
     args = parser.parse_args()
     num_points = args.num_points
 
     logging.info("Starting Heatmap Generation")
-    coordinates = [(lat, lon) for lat in np.linspace(LAT_MIN, LAT_MAX, num=num_points) 
-                               for lon in np.linspace(LON_MIN, LON_MAX, num=num_points)]
+    coordinates = [(lat, lon)
+                   for lat in np.linspace(LAT_MIN, LAT_MAX, num=num_points)
+                   for lon in np.linspace(LON_MIN, LON_MAX, num=num_points)]
 
     weather_data = []
-    for lat, lon in tqdm.tqdm(coordinates, desc="Fetching Weather Data"):
+    scalar_values = []
+    for lat, lon in tqdm.tqdm(coordinates, desc="Processing"):
         data = fetch_weather_data(lat, lon)
         weather_data.append(data)
-    
-    scalar_values = [compute_scalar_values(data) for data in weather_data]
-    interpolated_values = interpolate_values(coordinates, scalar_values, num_points)
+        scalar_value, avg_temp, avg_sun_exposure = compute_scalar_values(data)
+        logging.info(
+            f"Grid Point ({lat}, {lon}) - Avg Temp: {avg_temp:.2f}°F, Sun Exposure: {avg_sun_exposure:.2f}"
+        )
+        # Store or process the scalar value as needed
+        scalar_values.append(scalar_value)
+
+    interpolated_values = interpolate_values(coordinates, scalar_values,
+                                             num_points)
 
     center_lat = (LAT_MIN + LAT_MAX) / 2
     center_lon = (LON_MIN + LON_MAX) / 2
@@ -133,5 +156,5 @@ def main():
     heatmap.save('heatmap.html')
     logging.info("Heatmap Generated and Saved")
 
-main()
 
+main()
